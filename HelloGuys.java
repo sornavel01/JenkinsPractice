3d00df911663b818face85254ae994b2bafafb75
sornavel01 <59308988+sornavel01@users.noreply.github.com>
2020-07-12 16:57:21 +0530

    first commit

---
+++ HelloGuys.java
@@ -0,0 +1,18 @@
+public class HelloGuys{
+
+
+public static void main(String[] args{
+
+System.out.println("hey Agni");
+
+System.out.println("hey Agni");
+
+System.out.println("hey Ram");
+
+System.out.println("hey Kumar");
+
+System.out.println("hey Avan");
+
+}
+
+}
